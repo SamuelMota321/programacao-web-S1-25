bd9d54ca0f4d2b231229a80f9e5da2c1e3435d74
Samuel Rodrigues da Mota <138218205+SamuelMota321@users.noreply.github.com>
2025-03-05 10:21:35 -0300

    att1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,38 @@
+let num_A = 10;
+let num_B = 20;
+
+let soma = (num_A + num_B);
+let subtracao = (num_A - num_B);
+let multi = (num_A / num_B);
+let divisao = (num_A * num_B);
+
+console.log (`Resultado da Soma e: ${soma}`)
+console.log (`Resultado da Soma e: ${subtracao}`)
+console.log (`Resultado da Soma e: ${multi}`)
+console.log (`Resultado da Soma e: ${divisao}`)
+
+
+// const readline = require("readline")
+
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// })
+
+// rl.question("Digite o valor 1: ", function (input1) {
+//     rl.question("Digite o valor 2: ", function (input2) {
+//         const var1 = parseFloat(input1)
+//         const var2 = parseFloat(input2)
+
+//         if (isNaN(var1) || isNaN(var2)) {
+//             console.log("Por favor, insira números válidos.")
+//         } else {
+//             console.log(`A soma de ${var1} + ${var2} é: ${var1 + var2}`)
+//             console.log(`A subtração de ${var1} - ${var2} é: ${var1 - var2}`)
+//             console.log(`A divisão de ${var1} / ${var2} é: ${var1 / var2}`)
+//             console.log(`A multiplicação de ${var1} * ${var2} é: ${var1 * var2}`)
+//         }
+
+//         rl.close()
+//     })
+// })
